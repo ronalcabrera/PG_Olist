09ba5623ed11e76a800e1a15d2f516c9de665155
Maciela <mmacielaortiz@gmail.com>
2023-01-19 21:08:13 -0300

    primer intento

--- app/funciones_etl.py
+++ app/funciones_etl.py
@@ -8,10 +8,14 @@ import numpy as np
 def products_etl():
     # Ingestamos dataset
     url = 'https://raw.githubusercontent.com/ronalcabrera/PG_Olist/main/Datasets/olist_products_dataset.csv'
-    products = pd.read_csv(url, delimiter=',', encoding='UTF-8')    
+    products = pd.read_csv(url, delimiter=',', encoding='UTF-8')   
+    # Converimos el nombre de las columnas en minúsculas y eliminamos espacios
+    products.columns=products.columns.str.lower().strip()
     # Ingesto el dataset de categorias en inglés, para comparar
     url = 'https://raw.githubusercontent.com/ronalcabrera/PG_Olist/main/Datasets/product_category_name_translation.csv'
     cat_ingles = pd.read_csv(url, delimiter=',', encoding='UTF-8')
+    # Converimos el nombre de las columnas en minúsculas y eliminamos espacios
+    cat_ingles.columns=cat_ingles.columns.str.lower().strip()
     # Vamos a proceder a agregar estas categorías:
     cat_ingles.loc[len(cat_ingles)] = ['portateis_cozinha_e_preparadores_de_alimentos', 'kitchen_and_food_preparation_racks']
     cat_ingles.loc[len(cat_ingles)] = ['pc_gamer', 'pc_gamer']
@@ -30,6 +34,8 @@ def sellers_etl():
     # Ingestamos dataset
     url = 'https://raw.githubusercontent.com/ronalcabrera/PG_Olist/main/Datasets/olist_sellers_dataset.csv'
     sellers = pd.read_csv(url, delimiter=',', encoding='UTF-8')
+    # Converimos el nombre de las columnas en minúsculas y eliminamos espacios
+    sellers.columns=sellers.columns.str.lower().strip()
     # Creamos dataset limpio
     return sellers
 
@@ -38,6 +44,8 @@ def customers_etl():
     # Ingestamos dataset
     url = 'https://raw.githubusercontent.com/ronalcabrera/PG_Olist/main/Datasets/olist_customers_dataset.csv'
     customers = pd.read_csv(url, delimiter=',', encoding='UTF-8')
+    # Converimos el nombre de las columnas en minúsculas y eliminamos espacios
+    customers.columns=customers.columns.str.lower().strip()
     return customers
 
 #Limpieza del dataset olist_orders_dataset
@@ -45,6 +53,8 @@ def orders_etl():
     # Ingestamos dataset
     url = 'https://raw.githubusercontent.com/ronalcabrera/PG_Olist/main/Datasets/olist_orders_dataset.csv'
     orders = pd.read_csv(url, delimiter=',', encoding='UTF-8')
+    # Converimos el nombre de las columnas en minúsculas y eliminamos espacios
+    orders.columns=orders.columns.str.lower().strip()
     # Cambiamos los tipos de datos para las fechas
     orders['order_purchase_timestamp'] = pd.to_datetime(orders['order_purchase_timestamp'], format='%Y/%m/%d')
     orders['order_approved_at'] = pd.to_datetime(orders['order_approved_at'], format='%Y/%m/%d')
@@ -58,6 +68,8 @@ def geolocation_etl():
     # Ingestamos dataset
     url = 'https://raw.githubusercontent.com/ronalcabrera/PG_Olist/main/Datasets/olist_geolocation_dataset.csv'
     geolocation = pd.read_csv(url, delimiter=',', encoding='UTF-8')
+    # Converimos el nombre de las columnas en minúsculas y eliminamos espacios
+    geolocation.columns=geolocation.columns.str.lower().strip()
     return geolocation
 
 # Limpieza del dataset olist_marketing_qualified_leads_dataset
@@ -65,6 +77,8 @@ def marketing_qualified_leads_etl():
     # Ingestamos dataset
     url='https://raw.githubusercontent.com/ronalcabrera/PG_Olist/main/Datasets/olist_marketing_qualified_leads_dataset.csv'
     marketing_qualified_leads=pd.read_csv(url, delimiter=',', encoding='UTF-8')
+    # Converimos el nombre de las columnas en minúsculas y eliminamos espacios
+    marketing_qualified_leads.columns=marketing_qualified_leads.columns.str.lower().strip()
     # En la columna 'origin' reemplamos los NaN por 'Sin Dato'
     marketing_qualified_leads['origin']=marketing_qualified_leads['origin'].replace(np.nan,'Sin Dato')
     # Cambiaremos el tipo de dato de la columna 'first_contact_date'
@@ -75,7 +89,9 @@ def marketing_qualified_leads_etl():
 def order_items_etl():
     # Ingestamos dataset
     url = 'https://raw.githubusercontent.com/ronalcabrera/PG_Olist/main/Datasets/olist_order_items_dataset.csv'
-    df_order_items = pd.read_csv(url, delimiter=',', encoding='UTF-8')   
+    df_order_items = pd.read_csv(url, delimiter=',', encoding='UTF-8')
+    # Converimos el nombre de las columnas en minúsculas y eliminamos espacios
+    df_order_items.columns=df_order_items.columns.str.lower().strip()   
     # Cambiar la columna shipping_limit_date a tipo datetime
     df_order_items['shipping_limit_date'] = pd.to_datetime(df_order_items['shipping_limit_date'])
     return df_order_items
@@ -85,6 +101,8 @@ def order_payments_etl():
     # Ingestamos dataset
     url='https://raw.githubusercontent.com/ronalcabrera/PG_Olist/main/Datasets/olist_order_payments_dataset.csv'
     payments= pd.read_csv(url, delimiter=',', encoding='UTF-8') 
+    # Converimos el nombre de las columnas en minúsculas y eliminamos espacios
+    payments.columns=payments.columns.str.lower().strip()
     return payments
 
 #Limpieza del dataset olist_order_reviews_dataset
@@ -92,6 +110,8 @@ def order_reviews_etl():
     # Ingestamos dataset
     url='https://raw.githubusercontent.com/ronalcabrera/PG_Olist/main/Datasets/olist_order_reviews_dataset.csv'
     reviews=pd.read_csv(url, delimiter=',', encoding='UTF-8')
+    # Converimos el nombre de las columnas en minúsculas y eliminamos espacios
+    reviews.columns=reviews.columns.str.lower().strip()
     # Reemplazo los NaN por 'Sin Dato'
     reviews['review_comment_title'] = reviews['review_comment_title'].replace(np.nan,'Sin Dato')
     # Reemplazo los saltos de lineas por ' ' 
@@ -116,6 +136,8 @@ def closed_deals_etl():
     # Ingestamos dataset
     url = 'https://raw.githubusercontent.com/ronalcabrera/PG_Olist/main/Datasets/olist_closed_deals_dataset.csv'
     df_closed_deals = pd.read_csv(url, delimiter=',', encoding='UTF-8') 
+    # Converimos el nombre de las columnas en minúsculas y eliminamos espacios
+    df_closed_deals.columns=df_closed_deals.columns.str.lower().strip()
     df_closed_deals.drop_duplicates() # No hay duplicados
     # Cambiar la columna won_date a tipo datetime
     df_closed_deals['won_date'] = pd.to_datetime(df_closed_deals['won_date']) 
@@ -184,6 +206,65 @@ def zip_code_prefix_etl():
                                         np.where((czcp>89999)&(czcp<100000),'SC',
                                         'S/D' # Si no esta en el rango, sin datos.
                                         )))))))))))))))))))))))))))))))
+    zcpc = zip_code_prefix.customer_state.values
+
+    zip_code_prefix['lat'] =    np.where(zcpc=='SP',-9.974,
+                                np.where(zcpc=='RJ',-22.90642,
+                                np.where(zcpc=='ES',-22.11583,
+                                np.where(zcpc=='MG',-21.235,
+                                np.where(zcpc=='BA',-12.97111,
+                                np.where(zcpc=='SE',-10.91111,
+                                np.where(zcpc=='PE',-8.05389,
+                                np.where(zcpc=='AL',-9.66583,
+                                np.where(zcpc=='PB',-7.115,
+                                np.where(zcpc=='RN',-5.795,
+                                np.where(zcpc=='CE',-3.71722,
+                                np.where(zcpc=='PI',-5.08917,
+                                np.where(zcpc=='MA',-2.52972,
+                                np.where(zcpc=='PA',-1.45583,
+                                np.where(zcpc=='AP',0.03889,
+                                np.where(zcpc=='AM',-3.10194,
+                                np.where(zcpc=='RR',2.81972,
+                                np.where(zcpc=='AC',-9.97472,
+                                np.where(zcpc=='DF',-15.77972,
+                                np.where(zcpc=='GO',-16.67861,
+                                np.where(zcpc=='RO',-8.76194,
+                                np.where(zcpc=='TO',-10.16745,
+                                np.where(zcpc=='MT',-15.59611,
+                                np.where(zcpc=='MS',-20.44278,
+                                np.where(zcpc=='PR',-25.42778,
+                                np.where(zcpc=='SC',-27.59667,
+                                'S/D'
+                                ))))))))))))))))))))))))))
+
+    zip_code_prefix['long'] =   np.where(zcpc=='SP',-67.8076974,
+                                np.where(zcpc=='RJ',-43.18223,
+                                np.where(zcpc=='ES',-46.68278,
+                                np.where(zcpc=='MG',-45.75861,
+                                np.where(zcpc=='BA',-38.51083,
+                                np.where(zcpc=='SE',-37.07167,
+                                np.where(zcpc=='PE',-34.88111,
+                                np.where(zcpc=='AL',-35.73528,
+                                np.where(zcpc=='PB',-34.86306,
+                                np.where(zcpc=='RN',-35.20944,
+                                np.where(zcpc=='CE',-38.54306,
+                                np.where(zcpc=='PI',-42.80194,
+                                np.where(zcpc=='MA',-44.30278,
+                                np.where(zcpc=='PA',-48.50444,
+                                np.where(zcpc=='AP',-51.06639,
+                                np.where(zcpc=='AM',-60.025,
+                                np.where(zcpc=='RR',-60.67333,
+                                np.where(zcpc=='AC',-67.81,
+                                np.where(zcpc=='DF',-47.92972,
+                                np.where(zcpc=='GO',-49.25389,
+                                np.where(zcpc=='RO',-63.90389,
+                                np.where(zcpc=='TO',-48.32766,
+                                np.where(zcpc=='MT',-56.09667,
+                                np.where(zcpc=='MS',-54.64639,
+                                np.where(zcpc=='PR',-49.27306,
+                                np.where(zcpc=='SC',-48.54917,
+                                'S/D'
+                                ))))))))))))))))))))))))))
     return zip_code_prefix
 
 
--- dockerfile
+++ dockerfile
@@ -1,4 +1,11 @@
 FROM tiangolo/uvicorn-gunicorn-fastapi
+
+RUN pip install pymysql
+RUN pip install sqlalchemy
 RUN pip install pandas
+RUN pip install numpy
+RUN pip install sendmail 
+RUN pip install requests
+
 EXPOSE 80
 COPY ./app /app
